73a13f36520552c5aac9a6bdfc2834b9def0818d
chandinipriyadusetty-glitch <chandinipriyadusetty@gmail.com>
2026-08-25 21:32:39 +0530

    Add Banking System Project
    Initial implementation of a banking system with features for account creation, deposits, withdrawals, and balance inquiries.

---
+++ Task4_Banking_System/banking_system.c
@@ -0,0 +1,15 @@
+# CodeAlpha Internship - Task 4: Banking System
+
+## Features
+- Create Account
+- Deposit
+- Withdraw
+- Balance Enquiry
+
+## Concepts Used
+- Structures
+- Functions
+- File Handling
+
+## Language
+C Programming
